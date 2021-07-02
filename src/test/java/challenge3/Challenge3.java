f8778f73e36fae3b02713523d0b2ce319fd8ae2f
Mentalclear <mentalclear@gmail.com>
2021-07-02 14:01:03 -0400

    Cleaned up. Removed redundant import.

--- src/test/java/challenge3/Challenge3.java
+++ src/test/java/challenge3/Challenge3.java
@@ -11,7 +11,6 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
