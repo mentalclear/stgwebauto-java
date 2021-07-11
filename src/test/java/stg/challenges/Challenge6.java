09eb0d434876fe9347c6daff96ed0a1675fa5f24
Mentalclear <mentalclear@gmail.com>
2021-07-11 07:11:35 -0400

    Imports optimized in Challenge 6

--- src/test/java/stg/challenges/Challenge6.java
+++ src/test/java/stg/challenges/Challenge6.java
@@ -1,7 +1,9 @@
 package stg.challenges;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -10,10 +12,7 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import stg.utils.Helpers;
 
-import java.io.File;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class Challenge6 {
     public WebDriver driver;
