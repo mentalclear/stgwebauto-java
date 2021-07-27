a6afa5d0bd243ab285bf548af181b2fc92555158
Mentalclear <mentalclear@gmail.com>
2021-07-26 21:45:14 -0400

    Challenge 9 Completed! Starting with Challenge 10 next.

--- src/test/java/stg/challenges/Challenge9.java
+++ src/test/java/stg/challenges/Challenge9.java
@@ -1,6 +1,7 @@
 package stg.challenges;
 
-import kong.unirest.Unirest;
+import io.restassured.http.Cookies;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -9,17 +10,17 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
-import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.path.json.JsonPath.from;
 
 public class Challenge9 {
     public WebDriver driver;
     public WebDriverWait driverWait;
-
-    // Challenge 9 for Advanced STG certification
-    // Using Unirest here.
 
     @BeforeSuite
     public void startSuite() {
@@ -30,87 +31,37 @@ public class Challenge9 {
     }
 
     @Test(priority = 1)
-    public void logModelResponseBodyToFile()  {
-        String endpointURL = "https://www.copart.com/public/lots/search";
-
+    public void verifyThatCopartIsOpened(){
         driver.get("https://copart.com");
         Assert.assertEquals(driver.getTitle(),
                 "Salvage Cars for Sale | Online Used Car Auctions - Copart Auto Auction");
-
-        List<String> modelsList = new ArrayList<>(Arrays.asList(
-                "Toyota Camry",
-                "Nissan Skyline",
-                "Subaru Impreza",
-                "Mitsubishi Lancer EVO",
-                "Mercedes-Benz s500",
-                "Ford Mustang GT",
-                "Chevrolet Corvette",
-                "Ford Raptor",
-                "Toyota Supra",
-                "Lamborghini Urus"
-        ));
-        for (String model : modelsList) {
-           Assert.assertEquals(testForServerResponse(model, endpointURL), 200);
-           Assert.assertTrue(resultRequestToFile(model, endpointURL).isFile());
-           verifyingTheResponse(model, endpointURL);
-        }
     }
 
-    private int testForServerResponse(String searchQuery, String endpoint) {
-        return Unirest.post(endpoint)
-                .header("accept", "application/json")
-                .queryString("query", searchQuery)
-                .asEmpty()
-                .getStatus();
-    }
+    @Test (priority = 2)
+    public void getValueTypeForEachKey() {
+        String endpointURL = "https://www.copart.com/public/lots/search";
 
-    public File resultRequestToFile(String searchQuery, String endpoint) {
-        String searchQueryFileName = "./report/files/"
-                + searchQuery.toLowerCase().replaceAll("\\s", "_") + ".json";
-
-        // Removing previously created data files if exist, Unirest will not rewrite them.
-        File dataFile = new File(searchQueryFileName);
-        if(dataFile.isFile()) {
-            System.out.println("Previous instance of " + searchQueryFileName + " has been found in the target folder");
-            System.out.println("Deleting before continue returns: " + dataFile.delete());
-        } else {
-            System.out.println("Request data will be dumped into file: " + searchQueryFileName);
+        Set<Cookie> seleniumCookies = driver.manage().getCookies();
+        List<io.restassured.http.Cookie> restAssuredCookies = new ArrayList<>();
+        for (org.openqa.selenium.Cookie cookie : seleniumCookies) {
+            restAssuredCookies.add(new io.restassured.http.Cookie.Builder(cookie.getName(), cookie.getValue()).build());
         }
 
-        File result = Unirest.post(endpoint)
+        String response = given()
+                .cookies(new Cookies(restAssuredCookies))
                 .header("accept", "application/json")
-                .queryString("query", searchQuery)
-                .asFile(searchQueryFileName)
-                .getBody();
-        System.out.println("Request data saved into a file: " + result);
+                .queryParam("query", "Mitsubishi Lancer EVO")
+                .when()
+                .post(endpointURL).asString();
 
-        return result;
-    }
-
-    public void verifyingTheResponse(String searchQuery, String endpoint) {
-        String result = Unirest.post(endpoint)
-                .header("accept", "application/json")
-                .queryString("query", searchQuery)
-                .asString()
-                .getBody();
-        // System.out.println(result);
-
-        // parsing file "JSONExample.json"
-//        Object obj = new JSONParser().parse(result);
-//
-//        // typecasting obj to JSONObject
-//        JSONObject jo = (JSONObject) obj;
-//
-//        // getting lotNumberStr
-//        String lotNumberStr = (String) jo.get("lotNumberStr");
-//
-//        System.out.println(lotNumberStr);
-
+        Map<Object, Object> allItems = from(response).getMap("data.results.content[0]");
+        for (Map.Entry<Object,Object> item : allItems.entrySet())
+            System.out.println("Key: " + item.getKey() +
+                    ", Value is a: " + item.getValue().getClass().getSimpleName());
     }
 
     @AfterSuite
     public void stopSuite() {
        driver.quit();
-       Unirest.shutDown();
     }
 }
