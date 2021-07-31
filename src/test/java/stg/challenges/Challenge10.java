27e487d95c399617a1bf784354d514060973bd2f
Mentalclear <mentalclear@gmail.com>
2021-07-31 11:11:14 -0400

    Challenge 10 - Started. Added a test with DataProvider. Next step is to write actual test using that. I have an idea to try the same thing with JUnit.

--- src/test/java/stg/challenges/Challenge10.java
+++ src/test/java/stg/challenges/Challenge10.java
@@ -1,5 +1,41 @@
 package stg.challenges;
 
+import com.opencsv.CSVIterator;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 public class Challenge10 {
-    // TODO Follow the STG Certification doc
+
+    @DataProvider(name = "test")
+    public Iterator<Object[]> provider(){
+        List<Object[]> testCases = new ArrayList<>();
+        String[] data;
+        try {
+            CSVIterator iterator = new CSVIterator(new CSVReader
+                    (new FileReader("src/test/resources/cars.csv")));
+            while (iterator.hasNext()){  // While there are lines in the .csv
+                data = iterator.next();  // add a line that was read to the array "data"
+                testCases.add(data);     // To List of Object[] add an array as an element
+            }
+        } catch (CsvValidationException | IOException e) {
+            e.printStackTrace();
+        }
+        return testCases.iterator();  // Return the Iterator of Object[]
+    }
+
+    @Test(dataProvider = "test")
+    public void testWithDataProvider(String arg1, String arg2, String arg3, String arg4 ){
+        System.out.println(arg1 + " " + arg2 + " " + arg3 + " " + arg4);
+    }
+
+    // Thoughts: Using JUnit 5 this should be much simpler using @ParametrizedTest and @CsvFileSource
 }
